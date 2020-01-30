8de3e2f7f27122f8c9a35d5c77077cba18052cf0
eva montgomery <eva.m.montgomery@gmail.com>
2020-01-30 15:17:48 -0500

    adds more examples

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Card({text}) {
+
+    return (
+        <div>
+            <h2>{text}</h2>
+            <p>lalalalala</p>
+        </div>
+    )
+}
+
+
+
+export default Card;
---
+++ src/Greet.js
@@ -0,0 +1,22 @@
+// #1 import React
+import React from 'react';
+import Card from './Card';
+
+// #2 create a function
+function Greet({whom}) {
+ // React functions always return a single value written in HTML
+    // return (
+    //     <p>Hello, {whom}!</p>
+    //     );
+
+    const greetingText = `Hello, ${whom}`;
+
+    return (
+        <div>
+            <Card text={greetingText} />
+        </div>
+    );
+};
+
+// #3 export the function
+export default Greet;
